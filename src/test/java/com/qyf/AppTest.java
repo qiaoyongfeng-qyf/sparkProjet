92e4f7b87510517469a14bf02d64ae8eab3c2c66
qiaoyongfeng-qyf <1242487138@qq.com>
2020-08-16 20:29:07 +0800

    init project

---
+++ src/test/java/com/qyf/AppTest.java
@@ -0,0 +1,20 @@
+package com.qyf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
